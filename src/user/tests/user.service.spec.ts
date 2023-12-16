5d06e1906c017eb2fd3af33b65016f8fc59eb60c
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2023-12-17 01:39:08 +0530

    feat: implements the fetch calls of user service

--- src/user/tests/user.service.spec.ts
+++ src/user/tests/user.service.spec.ts
@@ -1,18 +1,61 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from '@/user/user.service';
+import { UserService } from '@/user/user.service'
+import { Test, TestingModule } from '@nestjs/testing'
+import { UserRepository } from '@/user/user.repository'
+import { User } from '../schemas/user.schema'
+import { userStub } from './stubs/user.stub'
+import { Types } from 'mongoose'
+
+jest.mock('../user.repository')
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService
+  let userRepository: UserRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+      imports: [],
+      providers: [UserService, UserRepository]
+    }).compile()
 
-    service = module.get<UserService>(UserService);
-  });
+    userRepository = module.get<UserRepository>(UserRepository)
+    userService = module.get<UserService>(UserService)
+    jest.clearAllMocks()
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(userService).toBeDefined()
+  })
+
+  describe('getAll is called', () => {
+    let users: Array<User>
+
+    beforeEach(async () => {
+      users = await userService.getAll()
+    })
+
+    it('should call userRepository', async () => {
+      expect(userRepository.find).toHaveBeenCalledWith({})
+    })
+
+    it('should return list of users', () => {
+      expect(users).toEqual([userStub()])
+    })
+  })
+
+  describe('getById', () => {
+    const userId: string = new Types.ObjectId().toHexString()
+    let user: User
+
+    beforeEach(async () => {
+      user = await userService.getById(userId)
+    })
+
+    it('should call userRepository', () => {
+      expect(userRepository.findOne).toHaveBeenCalledWith({ _id: new Types.ObjectId(userId) })
+    })
+
+    it('should return a user', () => {
+      expect(user).toEqual(userStub())
+    })
+  })
+})
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { UserRepository } from './user.repository'
+import { User } from './schemas/user.schema'
+import { Types } from 'mongoose'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  public constructor(private readonly userRepository: UserRepository) {}
+
+  public async getAll(): Promise<Array<User>> {
+    return await this.userRepository.find({})
+  }
+
+  public async getById(userId: string): Promise<User> {
+    const userObjectId = new Types.ObjectId(userId)
+
+    return await this.userRepository.findOne({ _id: userObjectId })
+  }
+}
