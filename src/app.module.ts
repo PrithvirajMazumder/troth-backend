417d9e00282e9cfbeae74cbd34fa681f91ce0916
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2023-12-16 19:33:18 +0530

    refactor: removes relative imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { UserModule } from '@/user/user.module';
 import { LoggerMiddleware } from '@/middlewares/logger.middleware';
-import { DbModule } from './db/db.module';
+import { DbModule } from '@/db/db.module';
 
 @Module({
   imports: [UserModule, DbModule],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { UserController } from '@/user/user.controller'
 import { UserService } from '@/user/user.service'
 import { User, UserSchema } from '@/user/schemas/user.schema'
-import { UserRepository } from './user.repository'
+import { UserRepository } from '@/user/user.repository'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
