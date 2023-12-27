bfe47dfd3e76c4474bc37379b384c48a93d0c6c8
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2023-12-27 19:45:55 +0530

    fix: adds user repository in the provider list in user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from '@/users/users.service'
 import { UsersResolver } from '@/users/users.resolver'
 import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from '@/users/schemas/user.schema'
+import { UsersRepository } from '@/users/users.repository'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
-  providers: [UsersResolver, UsersService, UsersResolver]
+  providers: [UsersResolver, UsersService, UsersRepository]
 })
 export class UsersModule {}
